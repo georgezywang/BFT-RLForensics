763954fcaaf83582e34cadb9ee5c47f5b56930cc
QIAOYI FANG <QF30@duke.edu>
2021-03-19 06:25:06 -0400

    fixed env_info

--- src/env/unfair_game/surviving.py
+++ src/env/unfair_game/surviving.py
@@ -200,7 +200,6 @@ class Surviving(MultiAgentEnv):
 
     def get_env_info(self):
         env_info = {
-                    "state_shape": self.get_state_size(),
                     "obs_shape": self.get_obs_size(),
                     "reward_shape": self.n_agents,
                     "n_actions": self.get_total_actions(),
