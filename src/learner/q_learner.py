54140ccf98d9fca00c7913d80aa97d96cdc811bf
QIAOYI FANG <QF30@duke.edu>
2021-03-19 10:02:46 -0400

    fixed keywords

--- src/learner/q_learner.py
+++ src/learner/q_learner.py
@@ -23,7 +23,7 @@ class QLearner:
 
     def train(self, batch: EpisodeBatch, t_env: int, episode_num: int):
         # Get the relevant quantities
-        rewards = batch["reward"][:, :-1]
+        rewards = batch["rewards"][:, :-1]
         actions = batch["actions"][:, :-1]
         terminated = batch["terminated"][:, :-1].float()
         mask = batch["filled"][:, :-1].float()
