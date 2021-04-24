4fea07c106d544edf9490cd843f9f15c60028343
QIAOYI FANG <QF30@duke.edu>
2021-04-24 17:21:42 -0400

    ahhhhhhh

--- src/learner/learner.py
+++ src/learner/learner.py
@@ -112,6 +112,7 @@ class SeparateLearner:
         pi = []
         for idx in range(num_action_types-1):
             out = th.stack([attacker_outs[t][idx] for t in range(len(attacker_outs))], dim=1)  # [bs, t, max_msg, num_action]
+            print(out.shape)
             out = th.gather(out[:, :-1], dim=3, index=attacker_actions[idx].unsqueeze(3)).squeeze(3)
             out[attacker_mask == 0] = 1.0
             pi.append(out)
