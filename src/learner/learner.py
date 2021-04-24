a94492eb5cd3c6c04641c64d6080fc3458640ab0
QIAOYI FANG <QF30@duke.edu>
2021-04-24 19:46:39 -0400

    ahhhhhhh

--- src/learner/learner.py
+++ src/learner/learner.py
@@ -252,7 +252,7 @@ class SeparateLearner:
                         parsed_actions_t_msg.append(parsed_actions[bs_idx][t_idx][msg_idx][-1][idx])
                     parsed_actions_t.append(parsed_actions_t_msg)
                 parsed_actions_idx.append(parsed_actions_t)
-            ret_cert.append(th.tensor(parsed_actions_idx, dtype=th.int64))
+            ret_cert.append(th.tensor(parsed_actions_idx, dtype=th.int64).to(device))
             # ret_cert.append(th.tensor(copy.deepcopy(parsed_actions_idx), dtype=th.long))
 
         ret.append(ret_cert)
