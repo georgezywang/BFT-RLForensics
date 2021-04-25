d0f77f9012203d21d51f9c17cca7dba2c7d15a96
QIAOYI FANG <QF30@duke.edu>
2021-04-25 02:23:38 -0400

    ahhhhhhh

--- src/learner/learner.py
+++ src/learner/learner.py
@@ -295,6 +295,7 @@ class SeparateLearner:
         # print("c id: {}".format(certificate_input))
         # print("c id: {}".format(list_rev_onehot(certificate_input)))
         # print("msg: {}".format(msg))
+        msg = th.cat(msg, dim=-1)
 
         return msg
 
